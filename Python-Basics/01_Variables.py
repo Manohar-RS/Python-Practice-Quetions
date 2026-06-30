ee26f870a4cf73be6924e202dc0114ba36ccaf0e
Mr. Manohar Rajendra Sonawane <manoharofficial027@gmail.com>
2026-06-30 22:42:07 +0530

    Add initial variable definitions and print statements

---
+++ Python-Basics/01_Variables.py
@@ -0,0 +1,13 @@
+# Topic       : Variables
+# Day         : 01
+
+
+# Creating Variables
+
+name = "Manohar"
+age = 22
+city = "Ahilyanagar"
+
+print("Name :", name)
+print("Age :", age)
+print("City :", city)
