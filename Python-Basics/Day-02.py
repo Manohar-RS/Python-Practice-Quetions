1e2692ed2a662fb9e00e2af0469ac80ec0ffbe43
Mr. Manohar Rajendra Sonawane <manoharofficial027@gmail.com>
2026-07-01 22:39:37 +0530

    Add Day-02.py with data types examples

---
+++ Python-Basics/Day-02.py
@@ -0,0 +1,20 @@
+# Topic       : Data Types
+# Day         : 02
+
+
+# Different Data Types
+
+name = "Manohar"
+age = 22
+height = 5.8
+is_student = True
+
+print("Name :", name)
+print("Age :", age)
+print("Height :", height)
+print("Student :", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
