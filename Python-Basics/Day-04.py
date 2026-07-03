8e3941c6440e9c8bacee41fb6b98dd292d81ebbe
Mr. Manohar Rajendra Sonawane <manoharofficial027@gmail.com>
2026-07-03 22:11:36 +0530

    Implement variable swapping program
    Added a program to swap two variables and print the results.

---
+++ Python-Basics/Day-04.py
@@ -0,0 +1,12 @@
+# Topic       : Variables
+# Day         : 04
+
+# Write a Python program to swap two variables.
+
+a = input("Enter the value of the first variable (a): ")
+b = input("Enter the value of the second variable (b): ")
+print(f"Original values: a = {a}, b = {b}")
+temp = a
+a = b
+b = temp
+print(f"Swapped values: a = {a}, b = {b}")
